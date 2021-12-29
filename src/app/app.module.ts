9f5f59bc147efb7d57ceadb63637066c2aa3f3d4
joesi8302 <joesi@cpp.edu>
2021-12-29 12:36:19 -0800

    Added page views and components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,43 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './pages/login/login.component';
+import { FeedComponent } from './pages/feed/feed.component';
+import { AccountComponent } from './pages/account/account.component';
+import { RegisterComponent } from './pages/register/register.component';
+import { NewPostComponent } from './components/new-post/new-post.component';
+import { ForgetPasswordComponent } from './pages/forget-password/forget-password.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { PostsComponent } from './components/posts/posts.component';
+import { MyInfoComponent } from './components/my-info/my-info.component';
+import { PublicInfoComponent } from './components/public-info/public-info.component';
+import { PostListComponent } from './components/post-list/post-list.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    FeedComponent,
+    AccountComponent,
+    RegisterComponent,
+    NewPostComponent,
+    ForgetPasswordComponent,
+    NavbarComponent,
+    PostsComponent,
+    MyInfoComponent,
+    PublicInfoComponent,
+    PostListComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
