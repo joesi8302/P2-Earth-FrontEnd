eb774b181bb5c6eadcca9294626d84a5d42f4484
joesi8302 <joesi@cpp.edu>
2022-01-04 16:01:53 -0800

    need to fix updateUser

--- src/app/models/User.ts
+++ src/app/models/User.ts
@@ -4,6 +4,6 @@ export interface User{
     password: string;
     user_first_name: string;
     user_last_name: string;
-    user_img: string;
+    user_img: File;
     user_email: string;
 }
--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -1,10 +1,54 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { User } from '../models/User';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ApiService {
 
+  domain: string = "http://localhost:9000";
+
   constructor(private httpCli: HttpClient) { }
+
+  login(user: User){
+    return this.httpCli.post(`${this.domain}/session`,{
+      "username": user.username,
+      "password": user.password
+    }, {withCredentials: true})
+  }
+
+  logout(){
+    return this.httpCli.post(`${this.domain}/session`, {withCredentials: true})
+  }
+
+  checkSession(){
+    return this.httpCli.get<any>(`${this.domain}/session`, {withCredentials: true})
+  }
+
+  registerUser(user: User){
+    return this.httpCli.post<any>(`${this.domain}/users`, {
+      "username":user.username,
+      "password": user.password,
+      "user_first_name": user.user_first_name,
+      "user_last_name": user.user_last_name,
+      "user_img": user.user_img,
+      "user_email": user.user_email
+    }, {withCredentials: true})
+  }
+
+  updateUser(user: User){
+    return this.httpCli.post<any>(`${this.domain}/users`, {
+      "username":user.username,
+      "password": user.password,
+      "user_first_name": user.user_first_name,
+      "user_last_name": user.user_last_name,
+      "user_img": user.user_img,
+      "user_email": user.user_email
+    }, {withCredentials: true})
+  }
+
+
+
+
 }
